cde0620f573401b5e09968f0aef241191ea9a4f8
kylepreston24 <W0443556@campus.nscc.ca>
2024-10-13 04:03:53 -0300

    Completed Inclass excercise 2

--- Lab2-HockeyTeam.py
+++ Lab2-HockeyTeam.py
@@ -9,9 +9,44 @@
 
 #Purpose/Concepts: Input and output to screen, string concatentation, string formatting, datatype casting, 
 # simple math operations
+"""
+Student Name:   Kyle Preston    
+Program Title:  HockeyTeam in class exericse 2  
+Description:   A code that prints a team name, win, loss, win percent with four decimal
+"""
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    #Variables
+    Wins= int(0) 
+    Losses= int(0)
+    Team = ""
+    Totalgames= int(0)
+    WinLossPct= float(0.0)
+
+
+    #Inputs
+    #Team name input
+    Team=input("Please enter team name: " )
+
+    #Win input
+    Wins=input("Please enter the amount of wins: ")
+
+    #Losses input
+    Losses=input("PLease enter the amount of losses: ")
+
+    #Calculations for the code
+    #Total games is wins + losses
+    # WinlossPct is wins divided by total games
+    
+    Totalgames= int(Wins) + int(Losses)
+    WinLossPct = int(Wins) / Totalgames
+
+    
+    #Output
+    # display team name with wins, losses and win percentage with four decimal places using .format
+    Output= "The {0} has {1} wins and {2} losses with a win percentage of {3:.4f}.".format(Team, Wins, Losses, WinLossPct)
+    print(Output)
 
 
 
